547902c71161deb09fe0d6949afbbd9360727a53
Sam(Gangmin) Bae <sbae000420@gmail.com>
2023-01-20 15:56:40 -0800

    first commit

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Player {
+    public int PlayerId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string?  Position { get; set; }
+
+    public double Salary { get; set; }
+
+    public string? TeamName { get; set; }
+
+    [ForeignKey("TeamName")]
+    public Team? Team { get; set; }
+}
